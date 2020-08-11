7ec1b62cbf5be6a159bc2ddfaef06d8a5465126f
Juan <jfilippo0@gmail.com>
2020-08-11 11:56:13 -0300

    Spring XML Configuration Ex

--- src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
@@ -4,12 +4,16 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
     // Configuration is used to consume whatever source isn't from yourself
     // them u need inform a Bean
 
-    @Bean
+    // if u gonna do the XML Config comment the @Configuration and the @Bean then
+    // create a file on Resources folder then write like my chuck-config.xml
+    // and import @ImportResource("classpath:your-file.xml") on main Application
+
+    //@Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
